3db5ccec2a941014e8200523555cf53a4d874466
Rob Blankson <robert.blankson@hudl.com>
2024-12-02 23:27:25 +0000

    Functional refactor
    Or at least an attempt!

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,59 +1,69 @@
-﻿// Part1();
+﻿global using LanguageExt;
+global using LanguageExt.Common;
+global using static LanguageExt.Prelude;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+Part1();
 Part2();
 
-
-void Part1()
+static void Part1()
 {
-    var listLeft = new List<int>();
-    var listRight = new List<int>();
-
     // var lines = File.ReadAllLines("testinput.txt");
     var lines = File.ReadAllLines("input.txt");
-    var length = lines.Length;
 
-    foreach (var line in lines)
-    {
-        var split = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-        listLeft.Add(int.Parse(split[0]));
-        listRight.Add(int.Parse(split[1]));
-    }
-
-    listLeft.Sort();
-    listRight.Sort();
-
-    var aggregate = 0;
-
-    for (int i = 0; i < length; i++)
-    {
-        aggregate += Math.Abs(listLeft[i] - listRight[i]);
-    }
+    var aggregate = ConvertLinesToLists(lines)
+        .Map((Left, Right) => ZipLists(Left, Right))
+        .Sum();
 
     Console.WriteLine(aggregate);
+
+    // Local functions
+    static (List<int> Left, List<int> Right) ConvertLinesToLists(string[] lines)
+        => lines
+            .Aggregate((Left: new List<int>(), Right: new List<int>()), PopulateLists);
+
+    static (List<int> Left, List<int> Right) PopulateLists((List<int> Left, List<int> Right) current, string line)
+    {
+        var split = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        current.Left.Add(int.Parse(split[0]));
+        current.Right.Add(int.Parse(split[1]));
+
+        return (current.Left, current.Right);
+    }
+
+    static IEnumerable<int> ZipLists(List<int> left, List<int> right) =>
+        left.Order()
+        .Zip(right.Order(), SumLists);
+    
+    static int SumLists(int left, int right) => Math.Abs(left - right);
 }
 
 static void Part2()
 {
     // var lines = File.ReadAllLines("testinput.txt");
     var lines = File.ReadAllLines("input.txt");
-    
-    var listLeft = new List<int>();
-    var dictRight = new Dictionary<int, int>();
 
-    foreach (var line in lines)
+    var similarity = ConvertLinesToLists(lines)
+        .Map((Left, Right) => Left.Aggregate(0, (current, left) => 
+        current + left * (Right.TryGetValue(left, out var count) ? count : 0)));
+
+    Console.WriteLine(similarity);
+
+    // Local functions
+    static (List<int> Left, Dictionary<int, int> Right) ConvertLinesToLists(string[] lines)
+        => lines
+            .Aggregate((Left: new List<int>(), Right: new Dictionary<int, int>()), PopulateCollections);
+
+    static (List<int> Left, Dictionary<int, int> Right) PopulateCollections((List<int> Left, Dictionary<int, int> Right) current, string line)
     {
         var split = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-        listLeft.Add(int.Parse(split[0]));
+        current.Left.Add(int.Parse(split[0]));
         var right = int.Parse(split[1]);
-        dictRight[right] = dictRight.TryGetValue(right, out var val) ? val + 1 : 1;
+        current.Right[right] = current.Right.TryGetValue(right, out var val) ? val + 1 : 1;
+
+        return (current.Left, current.Right);
     }
-
-    var aggregate = 0;
-
-    for (int i = 0; i < lines.Length; i++)
-    {
-        var left = listLeft[i];
-        aggregate += left * (dictRight.TryGetValue(left, out var count) ? count : 0);
-    }
-
-    Console.WriteLine(aggregate);
 }
