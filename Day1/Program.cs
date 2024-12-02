3b0dcd84fa55935bb4089482cf3292d5fdf9244d
Rob Blankson <robert.blankson@hudl.com>
2024-12-02 09:41:19 +0000

    Day1
    Imperative version

---
+++ Day1/Program.cs
@@ -0,0 +1,59 @@
+﻿// Part1();
+Part2();
+
+
+void Part1()
+{
+    var listLeft = new List<int>();
+    var listRight = new List<int>();
+
+    // var lines = File.ReadAllLines("testinput.txt");
+    var lines = File.ReadAllLines("input.txt");
+    var length = lines.Length;
+
+    foreach (var line in lines)
+    {
+        var split = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        listLeft.Add(int.Parse(split[0]));
+        listRight.Add(int.Parse(split[1]));
+    }
+
+    listLeft.Sort();
+    listRight.Sort();
+
+    var aggregate = 0;
+
+    for (int i = 0; i < length; i++)
+    {
+        aggregate += Math.Abs(listLeft[i] - listRight[i]);
+    }
+
+    Console.WriteLine(aggregate);
+}
+
+static void Part2()
+{
+    // var lines = File.ReadAllLines("testinput.txt");
+    var lines = File.ReadAllLines("input.txt");
+    
+    var listLeft = new List<int>();
+    var dictRight = new Dictionary<int, int>();
+
+    foreach (var line in lines)
+    {
+        var split = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        listLeft.Add(int.Parse(split[0]));
+        var right = int.Parse(split[1]);
+        dictRight[right] = dictRight.TryGetValue(right, out var val) ? val + 1 : 1;
+    }
+
+    var aggregate = 0;
+
+    for (int i = 0; i < lines.Length; i++)
+    {
+        var left = listLeft[i];
+        aggregate += left * (dictRight.TryGetValue(left, out var count) ? count : 0);
+    }
+
+    Console.WriteLine(aggregate);
+}
