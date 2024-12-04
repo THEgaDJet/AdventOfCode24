c7273dece9e3faf5c78005509d186bad9de2b089
Rob Blankson <robert.blankson@hudl.com>
2024-12-04 15:21:08 +0000

    Day3
    Part 1 & 2
    
    Imperative

---
+++ Day3/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Text.RegularExpressions;
+
+//Part1();
+Part2();
+
+static void Part1()
+{
+    // var text = File.ReadAllText("testinput.txt");
+    var text = File.ReadAllText("input.txt");
+
+    var regex = new Regex(@"mul\(\d{1,3},\d{1,3}\)");
+    var regex2 = new Regex(@"\d+");
+
+    var matches = regex.Matches(text);
+    var multiplications = matches.Aggregate(0, (current, match) => 
+        {
+            var r = regex2.Matches(match.Value);
+            return current + int.Parse(r[0].Value) * int.Parse(r[1].Value);
+        });
+
+    Console.WriteLine(multiplications);
+
+    // Local functions
+    
+}
+
+static void Part2()
+{
+    // var text = File.ReadAllText("testinput2.txt");
+    var text = File.ReadAllText("input.txt");
+
+    var shouldAdd = true;
+
+    var regex = new Regex(@"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)");
+    var regex2 = new Regex(@"\d+");
+
+    var multiplications = 0;
+    var matches = regex.Matches(text);
+
+    foreach (Match match in matches)
+    {
+        switch (match.Value)
+        {
+            case "do()":
+                shouldAdd = true;
+                continue;
+            case "don't()":
+                shouldAdd = false;
+                continue;
+            default:
+                var r = regex2.Matches(match.Value);
+                var mult = shouldAdd ? int.Parse(r[0].Value) * int.Parse(r[1].Value) : 0;
+                multiplications += mult;
+                break;
+        }
+    }
+
+    Console.WriteLine(multiplications);
+}
