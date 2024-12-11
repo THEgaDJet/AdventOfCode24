56e124f717753924bc5f01667d02678f5b002e7b
Rob Blankson <robert.blankson@hudl.com>
2024-12-11 15:16:25 +0000

    Initial commit

---
+++ Day9/Program.cs
@@ -0,0 +1,57 @@
+﻿
+Part1();
+// Part2();
+
+static void Part1()
+{
+    var files = new List<int>();
+    var freeSpace = new List<int>();
+
+    //var text = File. ReadAllText("testinput.txt");
+    using (StreamReader sr = new("../../../testinput.txt"))
+    {
+        var isFile = true;
+        while (sr.Peek() >= 0)
+        {
+            var c = (char)sr.Read();
+            var val = (int)char.GetNumericValue(c);
+
+            if (isFile)
+            {
+                files.Add(val); 
+            }
+            else
+            { 
+                freeSpace.Add(val); 
+            }
+            isFile = !isFile;
+        }
+
+        Console.WriteLine(files.Count);
+        Console.WriteLine(files.Sum());
+
+        Console.WriteLine();
+
+        Console.WriteLine(freeSpace.Count);
+        Console.WriteLine(freeSpace.Sum());
+    }
+
+    // var text = File.ReadAllText("input.txt");
+
+    Console.WriteLine();
+
+    // Local functions
+    
+}
+
+static void Part2()
+{
+    // var text = File.ReadAllText("testinput.txt");
+    var text = File.ReadAllText("testinput2.txt");
+    // var text = File.ReadAllText("input.txt");
+
+    Console.WriteLine();
+
+    // Local functions
+    
+}
