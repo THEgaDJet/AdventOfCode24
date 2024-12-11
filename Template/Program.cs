fb717f8286edb4edb4af1f49a5caceb71ec07c95
Rob Blankson <robert.blankson@hudl.com>
2024-12-11 15:16:40 +0000

    Add template

---
+++ Template/Program.cs
@@ -0,0 +1,25 @@
+﻿Part1();
+// Part2();
+
+static void Part1()
+{
+    var text = File.ReadAllText("testinput.txt");
+    // var text = File.ReadAllText("input.txt");
+
+    Console.WriteLine();
+
+    // Local functions
+    
+}
+
+static void Part2()
+{
+    // var text = File.ReadAllText("testinput.txt");
+    var text = File.ReadAllText("testinput2.txt");
+    // var text = File.ReadAllText("input.txt");
+
+    Console.WriteLine();
+
+    // Local functions
+    
+}
