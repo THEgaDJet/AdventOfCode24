aa1aa85603f8dca000399c8b68dfb2f0866ec971
Rob Blankson <robert.blankson@hudl.com>
2024-12-12 08:24:55 +0000

    Update for part 2

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -1,5 +1,5 @@
-﻿Part1();
-// Part2();
+﻿// Part1();
+Part2();
 
 
 static void Part1()
@@ -12,7 +12,6 @@ static void Part1()
     var stones = text.Split(' ').Select(ulong.Parse).ToList();
     for (uint i = 0; i < blinks; i++)
     {
-        var newStones = new List<ulong>();
         var transformedStones = stones.Select(ApplyRules);
         stones = transformedStones.SelectMany(s => s).ToList();
     }
@@ -20,32 +19,48 @@ static void Part1()
     Console.WriteLine(stones.Count);
 
     // Local functions
-    static ulong[] ApplyRules(ulong originalValue)
-    {
-        if (originalValue == 0)
-        {
-            return [1];
-        }
-        else if (originalValue.ToString().Length % 2 == 0)
-        {
-            var digits = originalValue.ToString().Length / 2;
-            var left = originalValue.ToString()[..digits];
-            var right = originalValue.ToString().Substring(digits, digits);
-
-            return [ulong.Parse(left), ulong.Parse(right)];
-        } 
-
-        return [originalValue * 2024];
-    }
 }
 
 static void Part2()
 {
-    // var text = File.ReadAllText("testinput.txt");
-    var text = File.ReadAllText("testinput2.txt");
-    // var text = File.ReadAllText("input.txt");
+    var blinks = 75;
+    // Probably need to batch.
+    // Given it is simultaneous, apply the transforms to one stone at a time and sum the resulting lengths
 
-    Console.WriteLine();
+    var text = File.ReadAllText("../../../input.txt");
+    var stones = text.Split(' ').Select(ulong.Parse).ToList();
 
-    // Local functions
+    var sum = 0;
+    for (int i = 0; i < stones.Count; i++)
+    {
+        var stone = stones[i];
+        var newStones = new List<ulong>() {stone};
+        for (int j = 0; j < blinks; j++)
+        {
+            var transformedStones = newStones.Select(ApplyRules);
+            newStones = transformedStones.SelectMany(s => s).ToList();
+        }
+
+        sum += newStones.Count;
+    }
+
+    Console.WriteLine(sum);
+}
+
+static ulong[] ApplyRules(ulong originalValue)
+{
+    if (originalValue == 0)
+    {
+        return [1];
+    }
+    else if (originalValue.ToString().Length % 2 == 0)
+    {
+        var digits = originalValue.ToString().Length / 2;
+        var left = originalValue.ToString()[..digits];
+        var right = originalValue.ToString().Substring(digits, digits);
+
+        return [ulong.Parse(left), ulong.Parse(right)];
+    } 
+
+    return [originalValue * 2024];
 }
